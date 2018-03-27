d0f8b1e2896a63ab01fe809131da6a206f92317c
Kincade <kincadep@gmail.com>
2018-03-27 13:38:03 -0600

    functional endpoints

--- app/global.js
+++ app/global.js
@@ -1,10 +1,10 @@
 global.school = 'Montana State University';
 if (global.school === 'Montana State University') {
   global.food = 'http://universityaroundtown.com/wp-json/wp/v2/sld/491';
-  global.bars = 'http://universityaroundtown.com/wp-json/wp/v2/sld/491';
-  global.shops = 'http://universityaroundtown.com/wp-json/wp/v2/sld/491';
-  global.coffee = 'http://universityaroundtown.com/wp-json/wp/v2/sld/491';
-  global.personal = 'http://universityaroundtown.com/wp-json/wp/v2/sld/491';
-  global.specialty = 'http://universityaroundtown.com/wp-json/wp/v2/sld/491';
+  global.bars = 'http://universityaroundtown.com/wp-json/wp/v2/sld/504';
+  global.shops = 'http://universityaroundtown.com/wp-json/wp/v2/sld/503';
+  global.coffee = 'http://universityaroundtown.com/wp-json/wp/v2/sld/509';
+  global.personal = 'http://universityaroundtown.com/wp-json/wp/v2/sld/508';
+  global.specialty = 'http://universityaroundtown.com/wp-json/wp/v2/sld/506';
   global.ads = 'http://universityaroundtown.com/wp-json/wp/v2/ads?_embed';
 }
